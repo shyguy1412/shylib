78c6ed57fdface7c62c2cebbaf292fe91d054cb7
Shy <shyguyx1412@gmail.com>
2025-07-19 21:20:26 +0200

    add component template

---
+++ templates/Component/index.tsx
@@ -0,0 +1,12 @@
+import style from "./style.module.css";
+import { h } from "preact";
+import { memo } from "preact/compat";
+
+namespace COMPONENT {
+  export type Props = {
+  };
+}
+
+export const COMPONENT = memo(({ }: COMPONENT.Props) => {
+  return <div></div>;
+});
